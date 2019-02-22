4c7a96d6311ce106001eb3fcec34a6ba920f5116
dudkiewicz <dudkiewiczsylwester@gmail.com>
2019-02-22 05:33:33 +0100

    update server and client

--- src/client.go
+++ src/client.go
@@ -1,32 +1,58 @@
-
 package main
 
-
-import "net"
-import "fmt"
-import "bufio"
-import "os"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
 
 func main() {
 
-	// connect to this socket
-	conn, err := net.Dial("tcp", "127.0.0.1:8081")
-	if err != nil{
-		fmt.Println(err)
-		fmt.Println("Exit from main.")
+	if len(os.Args) < 2 {
+		fmt.Println("TCP port is required in args.")
 		os.Exit(1)
-		return
 	}
+
+	port := os.Args[1]
+	fmt.Println("Client try connect on " + port + " port.")
+
 	for {
 		// read in input from stdin
 		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Text to send: ")
+		fmt.Println("Text to send: ")
 		text, _ := reader.ReadString('\n')
+		splited := strings.Split(text[:len(text)-1], "^")
+
 		// send to socket
-		fmt.Fprintf(conn, text + "\n")
-		// listen for reply
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Print("Message from server: "+message)
+		sendMessage(port, splited)
 	}
 }
 
+func sendMessage(port string, message []string) {
+	for _, element := range message {
+		conn := getConnection(port)
+		//if len(element)==0{
+		//	continue
+		//}
+		fmt.Fprintf(conn, element+"\n")
+		// listen for reply
+		reply, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Message from server: " + reply)
+		conn.Close()
+	}
+
+}
+
+func getConnection(port string) net.Conn {
+	host := "127.0.0.1:" + port
+	// connect to this socket
+	conn, err := net.Dial("tcp", host)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("Client connected to " + host)
+	return conn
+}
--- src/server.go
+++ src/server.go
@@ -1,45 +1,59 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
 
-import "net"
-import "fmt"
-import "bufio"
-import "strings" // only needed below for sample processing
+const (
+	CONN_HOST      = "127.0.0.1"
+	CONN_PORT_2011 = "2011"
+	CONN_PORT_2012 = "2012"
+	CONN_TYPE      = "tcp"
+)
 
 func main() {
+	fmt.Println("Starting listening ... ")
+	go listeningOnPort(CONN_PORT_2011)
+	listeningOnPort(CONN_PORT_2012)
 
-	fmt.Println("Launching server...")
+}
 
-	// listen on all interfaces
-	ln, _ := net.Listen("tcp", ":8081")
-
-	// accept connection on port
-	conn, _ := ln.Accept()
-
-	fmt.Println("Connected server.")
-
-	// run loop forever (or until ctrl-c)
+func listeningOnPort(port string) {
+	// Listen for incoming connections.
+	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+port)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	// Close the listener when the application closes.
+	defer l.Close()
+	fmt.Println("Listening on " + CONN_HOST + ":" + port)
 	for {
-		fmt.Println(conn.RemoteAddr().Network() ,conn.RemoteAddr().String())
-		// will listen for message to process ending in newline (\n)
-		messages, _ := bufio.NewReader(conn).ReadString('\n')
-
-		// output mmessagesessage received
-		splited := strings.Split(messages,"^")
-
-		for _ , message := range splited {
-			if len(message) == 0 || message =="\n"{
-				continue
-			}
-			fmt.Println("Message Received:", string(message))
-			// sample process for string received
-			newmessage := strings.ToUpper(message)
-			// send new string back to client
-			conn.Write([]byte("message "))
-			conn.Write([]byte("= " +newmessage+" "))
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
 		}
-		conn.Write([]byte("\n"))
-
+		// Handle connections in a new goroutine.
+		go handleRequest(conn, port)
 	}
 }
 
+// Handles incoming requests.
+func handleRequest(conn net.Conn, port string) {
+
+	message, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+	fmt.Print("Message Received on port="+port+":", string(message))
+
+	// Send a response back to person contacting us.
+	conn.Write([]byte(message))
+	// Close the connection when you're done with it.
+	conn.Close()
+}
